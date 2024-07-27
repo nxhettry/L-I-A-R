be1037656657acb286427c5daf217f9d8e4f0af3
Nishan Gautam <144698632+nxhettry@users.noreply.github.com>
2024-07-27 18:05:07 +0545

    Update Drawer.jsx

--- LIAR/components/Drawer.jsx
+++ LIAR/components/Drawer.jsx
@@ -60,9 +60,9 @@ const Drawer = () => {
         id="drawer-contact"
         className={`${
           drawerIsOpen
-            ? "flex flex-col items-center justify-start bg-black"
+            ? "flex flex-col items-center justify-start "
             : "hidden"
-        } fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform transform bg-inherit w-80`}
+        } fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform transform bg-black w-80`}
         aria-labelledby="drawer-contact-label"
       >
         <h5
