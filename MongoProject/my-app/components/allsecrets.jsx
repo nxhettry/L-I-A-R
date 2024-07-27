700cbe35eee0698561e57619f89b89bf04be2e3d
Nishan Gautam <144698632+nxhettry@users.noreply.github.com>
2024-07-27 16:28:00 +0545

    Delete MongoProject/my-app directory

--- MongoProject/my-app/components/allsecrets.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const allsecrets = () => {
-  return (
-    <div>MONGODB_URI="mongodb+srv://madeforphotos355:XM0d1R7CpZ4o14WY@cluster0.qlhvtek.mongodb.net/"</div>
-  )
-}
-
-export default allsecrets
